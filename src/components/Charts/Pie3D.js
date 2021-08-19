350c2986cf654729219f8b5b30ef960c7e74d92e
pmk7 <72909830+pmk7@users.noreply.github.com>
2021-08-19 14:36:00 +0200

    third commit

--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -18,8 +18,6 @@ import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
 // Adding the chart and theme as dependency to the core fusioncharts
 ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
 
-
-
 // STEP 3 - Creating the JSON object to store the chart configurations
 
 
@@ -27,7 +25,7 @@ const ChartComponent = ({data}) => {
 
   const chartConfigs = {
     type: "pie3d", // The chart type
-    width: "400", // Width of the chart
+    width: "100%", // Width of the chart
     height: "400", // Height of the chart
     dataFormat: "json", // Data type
     dataSource: {
